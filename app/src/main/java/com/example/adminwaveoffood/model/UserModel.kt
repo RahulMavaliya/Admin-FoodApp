b6f2494685303e8707d2814251976b64968ce4f0
Rahul <rdmavaliya@gmail.com>
2024-07-23 14:02:37 +0530

    Models

---
+++ app/src/main/java/com/example/adminwaveoffood/model/UserModel.kt
@@ -0,0 +1,12 @@
+package com.example.adminwaveoffood.model
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+data class UserModel(
+    val name: String?= null,
+    val nameOfRestaurant: String?= null,
+    val email: String?= null,
+    val password: String?= null,
+    val phone: String?= null,
+    var address : String?= null
+)
