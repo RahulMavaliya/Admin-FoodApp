4deef0e94868069942e60cc1c68d20d2facaee2a
Rahul <rdmavaliya@gmail.com>
2024-07-23 14:01:49 +0530

    Adapter

---
+++ app/src/main/java/com/example/adminwaveoffood/adapter/MenuItemAdapter.kt
@@ -0,0 +1,86 @@
+package com.example.adminwaveoffood.adapter
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.adminwaveoffood.databinding.ItemItemBinding
+import com.example.adminwaveoffood.model.AllMenu
+import com.google.firebase.database.DatabaseReference
+
+class MenuItemAdapter(
+    private val context: Context,
+    private val menuList: ArrayList<AllMenu>,
+    databaseReference: DatabaseReference,
+    private val onDeleteClickListener : (position : Int) -> Unit
+) : RecyclerView.Adapter<MenuItemAdapter.AddItemViewHolder>() {
+
+    private val itemQuantities = IntArray(menuList.size) { 1 }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MenuItemAdapter.AddItemViewHolder {
+        val binding = ItemItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AddItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MenuItemAdapter.AddItemViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int {
+        return menuList.size
+    }
+
+    inner class AddItemViewHolder(private val binding: ItemItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.apply {
+                val quantity = itemQuantities[position]
+                val menuItem = menuList[position]
+                val uriString = menuItem.foodImage
+                val uri = Uri.parse(uriString)
+
+                foodNameTextView.text = menuItem.foodName
+                priceTextView.text = menuItem.foodPrice
+                Glide.with(context).load(uri).into(foodImageView)
+                quantityTextView.text = quantity.toString()
+
+                minusButton.setOnClickListener {
+                    decreaseQuantity(position)
+                }
+                plusButton.setOnClickListener {
+                    increaseQuantity(position)
+                }
+                deleteButton.setOnClickListener {
+                    onDeleteClickListener(position)
+                }
+            }
+        }
+
+        private fun deleteQuantity(position: Int) {
+            menuList.removeAt(position)
+            menuList.removeAt(position)
+            menuList.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, menuList.size)
+        }
+
+        private fun decreaseQuantity(position: Int) {
+            if (itemQuantities[position] > 1) {
+                itemQuantities[position]--
+                binding.quantityTextView.text = itemQuantities[position].toString()
+            }
+        }
+
+        private fun increaseQuantity(position: Int) {
+            if (itemQuantities[position] < 10) {
+                itemQuantities[position]++
+                binding.quantityTextView.text = itemQuantities[position].toString()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/adminwaveoffood/adapter/PendingOrderAdapter.kt
@@ -0,0 +1,80 @@
+package com.example.adminwaveoffood.adapter
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.adminwaveoffood.databinding.PendingOrderItemBinding
+
+class PendingOrderAdapter(
+    private val context: Context,
+    private val customerNames: MutableList<String>,
+    private val quantity: MutableList<String>,
+    private val foodImage: MutableList<String>,
+    private val itemClicked: OnItemClicked
+) : RecyclerView.Adapter<PendingOrderAdapter.PendingOrderViewHolder>() {
+
+    interface OnItemClicked {
+        fun onItemClickListener(position: Int)
+        fun onItemAcceptClickListener(position: Int)
+        fun onItemDispatchClickListener(position: Int)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PendingOrderViewHolder {
+        val binding =
+            PendingOrderItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PendingOrderViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return customerNames.size
+    }
+
+    override fun onBindViewHolder(holder: PendingOrderViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class PendingOrderViewHolder(private val binding: PendingOrderItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private var isAccepted = false
+        fun bind(position: Int) {
+            binding.apply {
+                customerName.text = customerNames[position]
+                pendingOrderQuantity.text = quantity[position]
+                val uriString = foodImage[position]
+                val uri = Uri.parse(uriString)
+                Glide.with(context).load(uri).into(orderedFoodImage)
+
+                orderedAcceptButton.apply {
+                    if (!isAccepted) {
+                        text = "Accept"
+                    } else {
+                        text = "Dispatch"
+                    }
+                    setOnClickListener {
+                        if (!isAccepted) {
+                            text = "Dispatch"
+                            isAccepted = true
+                            showToast("Order is accepted")
+                            itemClicked.onItemAcceptClickListener(position)
+                        } else {
+                            customerNames.removeAt(adapterPosition)
+                            notifyItemRemoved(adapterPosition)
+                            showToast("Order Is Dispatched")
+                            itemClicked.onItemDispatchClickListener(position)
+                        }
+                    }
+                }
+                itemView.setOnClickListener {
+                    itemClicked.onItemClickListener(position)
+                }
+            }
+        }
+
+        private fun showToast(message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
